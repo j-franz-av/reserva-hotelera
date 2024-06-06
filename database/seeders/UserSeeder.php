e18145dc796c0f034c0c1cc0dc2d65e94a11c050
franz <arquinigovalle@gmail.com>
2024-06-06 16:30:55 -0500

    cambios en la base de datos

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,9 +18,16 @@ class UserSeeder extends Seeder
     {
         // User::factory(5)->create();
         User::create([
-            'name' => 'Wailan Tirajoh',
-            'email' => 'wailantirajoh@gmail.com',
-            'password' => Hash::make('wailan'),
+            'name' => 'Javier Franz',
+            'email' => 'arquinigovalle@gmail.com',
+            'password' => Hash::make('franz123'),
+            'role' => 'Super',
+            'random_key' => Str::random(60),
+        ]);
+        User::create([
+            'name' => 'Yeremin',
+            'email' => 'yereminblas@gmail.com',
+            'password' => Hash::make('yeremin123'),
             'role' => 'Super',
             'random_key' => Str::random(60),
         ]);
